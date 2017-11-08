e0f8c65ee2271d945bb084a1e89e8ae76fea2fd6
guilhermefq <gfantuci@gmail.com>
2017-11-08 10:46:28 -0300

    Alteração da mensagem de erro no Adiciona usuário

--- Cliente-adiciona.php
+++ Cliente-adiciona.php
@@ -15,7 +15,7 @@ if ($clienteDAO->insereCliente($cliente)){
 	header("Location: Cliente-lista.php");
 	die();
 } else {
-	$_SESSION["danger"] = "Erro ao adicionar o Usuário!";
+	$_SESSION["danger"] = "Erro ao adicionar um novo Usuário!";
 	header("Location: Cliente-lista.php");
 	die();
 }
